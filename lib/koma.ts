302945b159872b08fa597ca87acad387c3d657dc
You Matsumoto <youmts22@gmail.com>
2021-11-23 16:22:02 +0900

    feat: Show koma

---
+++ lib/koma.ts
@@ -0,0 +1,6 @@
+export type KomaType = {
+  image: string
+}
+
+export const Fuhyo: KomaType = { image: "/images/syougi14_fuhyou.png" }
+export const Osho: KomaType = { image: "/images/syougi01_ousyou.png" }
--- pages/reach/game.tsx
+++ pages/reach/game.tsx
@@ -1,10 +1,50 @@
+import * as koma from "../../lib/koma"
+
+type Koma = {
+  type: koma.KomaType,
+  x: number // 0-8
+  y: number // 0-8
+  isAlly: boolean
+}
+
 function ShogiBan(props: {width: number, height: number}) {
+  const komaList: Koma[] = [
+    { type: koma.Fuhyo, x: 0, y: 0, isAlly: true},
+    { type: koma.Fuhyo, x: 1, y: 0, isAlly: false},
+    { type: koma.Osho, x: 8, y: 8, isAlly: false},
+    { type: koma.Fuhyo, x: 7, y: 8, isAlly: true},
+  ]
+
+  const komaAreaX: number = 25
+  const komaAreaY: number = 21
+
+  const komaWidth: number = 76
+  const komaHeight: number = 83.5
+
+  var komaItems = komaList.map(
+    (koma, index) => {
+      const left = komaAreaX + koma.x * komaWidth
+      const top = komaAreaY + koma.y * komaHeight
+      const transform = koma.isAlly ? "" : `rotate(180, ${left + komaWidth * 0.5 }, ${top + komaHeight * 0.5 + 2 })`
+      return (
+        // TODO: not use index
+        <image key={index} href={koma.type.image}
+          x={left}
+          y={top}
+          width={komaWidth} height={komaHeight}
+          transform={transform}
+        />
+      )
+    }
+  )
+
   return (
     <svg version="1.1"
       baseProfile="full"
       width={props.width} height={props.height}
       xmlns="http://www.w3.org/2000/svg">
       <image href="/images/shogi_ban.png" x="0" y="0" width={props.width} height={props.height} />
+      {komaItems}
     </svg>
   )
 }
@@ -14,6 +54,8 @@ export default function Game() {
   const height: number = 800
 
   return (
-    <ShogiBan width={width} height={height} />
+    <>
+      <ShogiBan width={width} height={height} />
+    </>
   )
 }
