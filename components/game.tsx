7e310c4df70f374fffafcb59166781c7a504de25
You Matsumoto <youmts22@gmail.com>
2021-11-27 15:03:43 +0900

    chore: use prettier

--- components/game.tsx
+++ components/game.tsx
@@ -1,17 +1,17 @@
-import React from "react"
-import * as koma from "../lib/koma"
-import styles from "./game.module.css"
+import React from 'react'
+import * as koma from '../lib/koma'
+import styles from './game.module.css'
 
-const gameWidth: number = 735
-const gameHeight: number = 800
+const gameWidth = 735
+const gameHeight = 800
 
-const komaAreaX: number = 25
-const komaAreaY: number = 21
+const komaAreaX = 25
+const komaAreaY = 21
 
-const komaWidth: number = 76
-const komaHeight: number = 83.5
+const komaWidth = 76
+const komaHeight = 83.5
 
-let komaIdMax: number = 0
+let komaIdMax = 0
 
 type Koma = {
   id: number
@@ -21,58 +21,66 @@ type Koma = {
   isAlly: boolean
 }
 
+function KomaPiece(props: { koma: Koma }) {
+  const { koma } = props
+  const left = komaAreaX + koma.x * komaWidth
+  const top = komaAreaY + koma.y * komaHeight
+  const transform = koma.isAlly
+    ? ''
+    : `rotate(180, ${left + komaWidth * 0.5}, ${top + komaHeight * 0.5 + 2})`
 
-function KomaPiece(props: {koma: Koma}) {
-    const {koma} = props
-    const left = komaAreaX + koma.x * komaWidth
-    const top = komaAreaY + koma.y * komaHeight
-    const transform = koma.isAlly ? "" : `rotate(180, ${left + komaWidth * 0.5 }, ${top + komaHeight * 0.5 + 2 })`
+  // TODO: delete this
+  function handleClick() {
+    alert(`clicked! : ${koma.type.image}`)
+  }
 
-    // TODO: delete this
-    function handleClick(event: React.MouseEvent<SVGElement, MouseEvent>) {
-      alert(`clicked! : ${koma.type.image}`)
-    }
-
-    return (
-      <image href={koma.type.image}
-        x={left}
-        y={top}
-        width={komaWidth} height={komaHeight}
-        transform={transform}
-        onClick={handleClick}
-      />
-    )
+  return (
+    <image
+      href={koma.type.image}
+      x={left}
+      y={top}
+      width={komaWidth}
+      height={komaHeight}
+      transform={transform}
+      onClick={handleClick}
+    />
+  )
 }
 
-function ShogiBan(props: {width: number, height: number}) {
+function ShogiBan(props: { width: number; height: number }) {
   const komaList: Koma[] = [
-    { id: komaIdMax++, type: koma.Fuhyo, x: 0, y: 0, isAlly: true},
-    { id: komaIdMax++, type: koma.Fuhyo, x: 1, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Kyosha, x: 2, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Keima, x: 3, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Ginsho, x: 4, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Kinsho, x: 5, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Kakugyo, x: 6, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Hisha, x: 7, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Gyokusho, x: 8, y: 0, isAlly: false},
-    { id: komaIdMax++, type: koma.Osho, x: 8, y: 8, isAlly: false},
-    { id: komaIdMax++, type: koma.Fuhyo, x: 7, y: 8, isAlly: true},
+    { id: komaIdMax++, type: koma.Fuhyo, x: 0, y: 0, isAlly: true },
+    { id: komaIdMax++, type: koma.Fuhyo, x: 1, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Kyosha, x: 2, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Keima, x: 3, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Ginsho, x: 4, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Kinsho, x: 5, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Kakugyo, x: 6, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Hisha, x: 7, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Gyokusho, x: 8, y: 0, isAlly: false },
+    { id: komaIdMax++, type: koma.Osho, x: 8, y: 8, isAlly: false },
+    { id: komaIdMax++, type: koma.Fuhyo, x: 7, y: 8, isAlly: true },
   ]
 
-  var komaItems = komaList.map(
-    (koma) => <KomaPiece key={koma.id} koma={koma} />
-  )
+  const komaItems = komaList.map((koma) => (
+    <KomaPiece key={koma.id} koma={koma} />
+  ))
 
   return (
     <>
-      <image href="/images/shogi_ban.png" x="0" y="0" width={props.width} height={props.height} />
+      <image
+        href="/images/shogi_ban.png"
+        x="0"
+        y="0"
+        width={props.width}
+        height={props.height}
+      />
       {komaItems}
     </>
   )
 }
 
 export default function Game() {
-
   const viewBox = `0 0 ${gameWidth} ${gameHeight}`
 
   return (
@@ -82,7 +90,8 @@ export default function Game() {
         version="1.1"
         baseProfile="full"
         viewBox={viewBox}
-        xmlns="http://www.w3.org/2000/svg">
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <ShogiBan width={gameWidth} height={gameHeight} />
       </svg>
     </div>
--- lib/koma.ts
+++ lib/koma.ts
@@ -2,12 +2,12 @@ export type KomaType = {
   image: string
 }
 
-export const Fuhyo: KomaType = { image: "/images/syougi14_fuhyou.png" }
-export const Kyosha: KomaType = { image: "/images/syougi12_kyousya.png" }
-export const Keima: KomaType = { image: "/images/syougi10_keima.png" }
-export const Ginsho: KomaType = { image: "/images/syougi08_ginsyou.png" }
-export const Kinsho: KomaType = { image: "/images/syougi07_kinsyou.png" }
-export const Kakugyo: KomaType = { image: "/images/syougi05_gakugyou.png" }
-export const Hisha: KomaType = { image: "/images/syougi03_hisya.png" }
-export const Gyokusho: KomaType = { image: "/images/syougi02_gyokusyou.png" }
-export const Osho: KomaType = { image: "/images/syougi01_ousyou.png" }
+export const Fuhyo: KomaType = { image: '/images/syougi14_fuhyou.png' }
+export const Kyosha: KomaType = { image: '/images/syougi12_kyousya.png' }
+export const Keima: KomaType = { image: '/images/syougi10_keima.png' }
+export const Ginsho: KomaType = { image: '/images/syougi08_ginsyou.png' }
+export const Kinsho: KomaType = { image: '/images/syougi07_kinsyou.png' }
+export const Kakugyo: KomaType = { image: '/images/syougi05_gakugyou.png' }
+export const Hisha: KomaType = { image: '/images/syougi03_hisya.png' }
+export const Gyokusho: KomaType = { image: '/images/syougi02_gyokusyou.png' }
+export const Osho: KomaType = { image: '/images/syougi01_ousyou.png' }
